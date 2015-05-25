b1939c0b4e02223744c728642ad3013d04ef2f6b
Dylan <dylancwood@gmail.com>
2015-05-25 14:04:44 -0600

    fix permissions and path for rep_restore

--- rep_restore/pg_restore_coins_db_driver.sh
+++ rep_restore/pg_restore_coins_db_driver.sh
@@ -6,5 +6,5 @@
 
 # Switch to the pg user:
 PG_SERVICE_USER=postgres
-SCRIPT_PATH=/coins/db_crons/rep_restore/restore_coins_db.sh
+SCRIPT_PATH=/coins/db_crons/rep_restore/pg_restore_coins_db.sh
 sudo -u $PG_SERVICE_USER $SCRIPT_PATH
