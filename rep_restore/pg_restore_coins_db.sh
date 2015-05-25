e232d2e2f37465cc23aa4882c26ada413b714590
Dylan <dylancwood@gmail.com>
2015-05-25 08:41:45 -0600

    added replication DB restore script

---
+++ rep_restore/pg_restore_coins_db.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Author: Dylan Wood
+# Date: May.15.2015
+# Script is called from /etc/cron.daily/pg_restore_coins_db_driver.sh.
+
+echo 'Starting COINS DB refresh as user:'
+echo `id`
+echo `date`
+
+# Define variables
+ARCHIVE_DIR="/coins/mnt/ni/prd-backups/hid/mrsdba/prd"
+ARCHIVE_FILE=`ls -1t $ARCHIVE_DIR | head -1`
+DBNAME="coins"
+
+# Drop coins database
+
+# First, disconnect all connections
+echo 'Terminating connections to DB'
+psql -d $DBNAME -c "SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE procpid <> pg_backend_pid() AND datname = '$DBNAME';"
+
+# Drop DB
+echo 'Dropping DB'
+dropdb $DBNAME
+
+# Create empty DB
+echo 'Creating empty DB'
+createdb $DBNAME
+
+# Restore DB
+echo 'Restoring DB from archive'
+pg_restore -d $DBNAME $ARCHIVE_DIR/$ARCHIVE_FILE
+
+# Edit default search path
+echo 'Setting default search path'
+psql -d $DBNAME -c 'ALTER DATABASE nirep SET search_path=mrsdba, casdba, public;'
+
+echo 'Finished with COINS DB refresh'
+echo `date`
---
+++ rep_restore/pg_restore_coins_db_driver.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Author: Dylan Wood
+# Date: May.15.2015
+# Script to be run daily (put in /etc/cron.daily/) to restore the coins db on this server from the latest backup.
+
+# Switch to the pg user:
+PG_SERVICE_USER=postgres
+SCRIPT_PATH=/coins/db_crons/rep_restore/restore_coins_db.sh
+sudo -u $PG_SERVICE_USER $SCRIPT_PATH
